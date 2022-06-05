79299dda3de17e1fadfa6c07f2d50fd838b0f61c
Guilherme Fernandes <72757771+FernandesGuilherme07@users.noreply.github.com>
2022-06-05 17:48:09 -0300

    Opportunities model

---
+++ src/models/Opportunities.ts
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const opportunitiesSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    about: {
+      type: String,
+      required: true,
+    },
+    responsibilities: {
+      type: String,
+      required: true,
+    },
+    benefits: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Opportunities", opportunitiesSchema);
